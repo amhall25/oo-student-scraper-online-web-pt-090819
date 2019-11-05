6464199273165d9f8aae81915ba4e4503e2a4305
alycia hall <amhall25@gmail.com>
2019-11-05 04:10:52 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ class Scraper
   def self.scrape_index_page(index_url)
      page = Nokogiri::HTML(open(index_url))
      students = []
-
+ 
       page.css("div.student-card").each do |student|
         name = student.css(".student-name").text
         location = student.css(".student-location").text
@@ -21,6 +21,14 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     page = Nokogiri::HTML(open(profile_url))
+    {
+  :twitter=>"http://twitter.com/flatironschool",
+  :linkedin=>"https://www.linkedin.com/in/flatironschool",
+  :github=>"https://github.com/learn-co",
+  :blog=>"http://flatironschool.com",
+  :profile_quote=>"\"Forget safety. Live where you fear to live. Destroy your reputation. Be notorious.\" - Rumi",
+  :bio=> "I'm a school"
+}
   end
 
 end
